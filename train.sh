e901226191ba22d7bed76f83688e959b076b88b4
aquadzn <williamjcqs8@gmail.com>
2020-02-20 10:41:27 +0100

    Changed train.sh

--- train.sh
+++ train.sh
@@ -21,8 +21,14 @@ nvidia-docker run --rm \
             -dont_show \
             -gpus 1,2
 
-# Example:
+# Train example:
 # nvidia-docker run --rm -it -v "$(pwd):/usr/src/app" -w "/usr/src/app" --runtime=nvidia \
     # --name train_darknet william/darknet:latest \
     # detector train data/obj.data cfg/customv3-tiny.cfg weights/yolov3-tiny.conv.11 -dont_show -gpus 1,2
+
+# Compute MAP:
+# nvidia-docker run -it --rm -v "$(pwd):/usr/src/app" -w "/usr/src/app" --runtime=nvidia --name train_darknet_tiny william/darknet detector map data/obj.data cfg/custom/customv3-tiny.cfg backup/customv3-tiny_final.weights -dont_show
+
+# Detect on test image:
+# nvidia-docker run -it --rm -v "$(pwd):/usr/src/app" -w "/usr/src/app" --runtime=nvidia --name train_darknet_tiny william/darknet detector test data/obj.data cfg/custom/customv3-tiny.cfg backup/customv3-tiny_final.weights samples/photo_demo_Ionel/test_2.jpg -dont_show
 
